8e11f6ad811f7321e7889a6595023d8165f21508
Scott Chamberlain <myrmecocystus@gmail.com>
2014-04-01 16:07:47 -0700

    added more options for data.frames and lists, passed checks

--- R/to_geojson.r
+++ R/to_geojson.r
@@ -2,66 +2,76 @@
 #' 
 #' @import sp rgdal
 #' @export
-#' @param input
+#' @param input Input list, data.frame, or spatial class
 #' @param lat Latitude name
 #' @param lon Longitude name
+#' @param polygon If a polygon is defined in a data.frame, this is the column that defines the grouping
+#' of the polygons in the data.frame
+#' @param output One of 'list' or 'geojson'
+#' @param ... Further args
+#' @examples
+#' library(maps)
+#' data(us.cities)
+#' head(us.cities)
+#' to_geojson(input=us.cities, lat='lat', lon='long')
+#' 
+#' # polygons
+#' library(ggplot2)
+#' states <- map_data("state")
+#' head(states)
+#' ## make list for input to e.g., rMaps
+#' res <- to_geojson(input=states, lat='lat', lon='long', group='group')
+#' ## make geojson from the list
+#' list_to_geojson.SpatialPolygonsDataFrame(res)
+#' 
+#' ## From a list
+#' mylist <- list(list(latitude=30, longitude=120, marker="red"), list(latitude=30, longitude=130, marker="blue"))
+#' to_geojson(mylist)
 
 to_geojson <- function(...){
   UseMethod("to_geojson")
 }
 
-#' @method to_geojson default
-#' @export
-#' @rdname to_geojson
-to_geojson.default <- function(input, lat = "latitude", lon = "longitude", ...){
-    x <- lapply(input, function(l) {
-        if (is.null(l[[lat]]) || is.null(l[[lon]])) {
-            return(NULL)
-        }
-        list(type = "Feature", geometry = list(type = "Point", coordinates = as.numeric(c(l[[lon]], 
-            l[[lat]]))), properties = l[!(names(l) %in% c(lat, lon))])
-    })
-    setNames(Filter(function(x) !is.null(x), x), NULL)
-}
-
 #' @method to_geojson data.frame
 #' @export
 #' @rdname to_geojson
-to_geojson.data.frame <- function(input, lat = "latitude", lon = "longitude", ...){
-  x <- lapply(input, function(l) {
-    if (is.null(l[[lat]]) || is.null(l[[lon]])) {
-      return(NULL)
-    }
-    list(type = "Feature", geometry = list(type = "Point", coordinates = as.numeric(c(l[[lon]], 
-                                                                                      l[[lat]]))), properties = l[!(names(l) %in% c(lat, lon))])
-  })
-  setNames(Filter(function(x) !is.null(x), x), NULL)
+to_geojson.data.frame <- function(input, lat = "latitude", lon = "longitude", polygon=NULL, output='list', ...){
+  if(output=='list'){
+    input <- apply(input, 1, as.list)
+    x <- lapply(input, function(l) {
+      if (is.null(l[[lat]]) || is.null(l[[lon]])) {
+        return(NULL)
+      }
+      type <- ifelse(is.null(polygon), "Point", "Polygon")
+      list(type = type,
+           geometry = list(type = "Point", 
+                           coordinates = as.numeric(c(l[[lon]], l[[lat]]))), 
+           properties = l[!(names(l) %in% c(lat, lon))])
+    })
+    setNames(Filter(function(x) !is.null(x), x), NULL)
+  } else {
+    res <- df_to_SpatialPolygonsDataFrame(input)
+    SpatialPolygonsDataFrame_togeojson(res)
+  }
 }
 
 #' @method to_geojson list
 #' @export
 #' @rdname to_geojson
-to_geojson.list <- function(input, lat = "latitude", lon = "longitude", ...){
-  x <- lapply(input, function(l) {
-    if (is.null(l[[lat]]) || is.null(l[[lon]])) {
-      return(NULL)
-    }
-    list(type = "Feature", geometry = list(type = "Point", coordinates = as.numeric(c(l[[lon]], 
-                                                                                      l[[lat]]))), properties = l[!(names(l) %in% c(lat, lon))])
-  })
-  setNames(Filter(function(x) !is.null(x), x), NULL)
-}
-
-#' @method to_geojson sp
-#' @export
-#' @rdname to_geojson
-to_geojson.sp <- function(input, lat = "latitude", lon = "longitude", ...){
-  x <- lapply(input, function(l) {
-    if (is.null(l[[lat]]) || is.null(l[[lon]])) {
-      return(NULL)
-    }
-    list(type = "Feature", geometry = list(type = "Point", coordinates = as.numeric(c(l[[lon]], 
-                                                                                      l[[lat]]))), properties = l[!(names(l) %in% c(lat, lon))])
-  })
-  setNames(Filter(function(x) !is.null(x), x), NULL)
+to_geojson.list <- function(input, lat = "latitude", lon = "longitude", output='list', ...){
+  if(output=='list'){
+    x <- lapply(input, function(l) {
+      if (is.null(l[[lat]]) || is.null(l[[lon]])) {
+        return(NULL)
+      }
+      type <- ifelse(is.null(polygon), "Point", "Polygon")
+      list(type = type,
+           geometry = list(type = "Point", 
+                           coordinates = as.numeric(c(l[[lon]], l[[lat]]))), 
+           properties = l[!(names(l) %in% c(lat, lon))])
+    })
+    setNames(Filter(function(x) !is.null(x), x), NULL)
+  } else {
+    list_to_geojson.SpatialPolygonsDataFrame(input)
+  }
 }
