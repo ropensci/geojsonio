6fb2e8ff610a798653f40583bca6979f93f87818
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-30 22:22:49 -0700

    started geojson_read fxn, #15

---
+++ R/geojson_read.R
@@ -0,0 +1,37 @@
+#' Read a geojson file
+#'
+#' @export
+#'
+#' @param file Path to a local file or a URL.
+#' @param ... Further args passed on to \code{\link[jsonlite]{toJSON}}
+#' @param x Ignored.
+#'
+#' @examples \dontrun{
+#' # From a file
+#' geojson_read("~/asdfafaf")
+#'
+#' # From a URL
+#' geojson_read('<URL>')
+#' }
+
+geojson_read <- function(...) UseMethod("geojson_read")
+
+#' @export
+#' @rdname geojson_read
+geojson_read.file <- function(input, ...) read_geojson(as.path(input))
+
+#' @export
+#' @rdname geojson_read
+geojson_read.url <- function(input, ...) read_geojson(as.path(input))
+
+#' @export
+#' @rdname geojson_read
+as.location <- function(x, ...) UseMethod("as.location")
+
+#' @export
+#' @rdname geojson_read
+as.location.file <- function(x, ...) as.location(x, ...)
+
+#' @export
+#' @rdname geojson_read
+as.location.url <- function(x, ...) as.location(x, ...)
