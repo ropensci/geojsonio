f241e8b1547d6e2269ac352ff30189c16017d328
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-05 17:14:01 -0800

    renamed some files

---
+++ R/geojsonio-package.r
@@ -0,0 +1,10 @@
+#' Convert various data formats to geoJSON or topoJSON
+#'
+#' This package converts lists, data.frame's, numeric, SpatialPolygons, and
+#' SpatialPolygonsDataFrame to geoJSON with the help of \code{rgdal} and friends, or topoJSON
+#' with the help of the npm node package \code{topojson}
+#'
+#' @name geojsonio
+#' @author Scott Chamberlain
+#' @docType package
+NULL
