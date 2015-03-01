a5dc77ea71081e584c94289916a7fd60cbddd03b
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-01 08:59:09 -0800

    started tests

---
+++ tests/testthat/test_as.location.R
@@ -0,0 +1,19 @@
+context("testing as.location")
+
+# A file
+file <- system.file("examples", "zillow_or.geojson", package = "togeojson")
+a <- as.location(file)
+
+# A URL
+url <- "https://raw.githubusercontent.com/glynnbird/usstatesgeojson/master/california.geojson"
+b <- as.location(url)
+
+test_that("as.location returns correct class", {
+  expect_is(a, "location")
+  expect_is(a[[1]], "character")
+  expect_null(names(a))
+  
+  expect_is(b, "location")
+  expect_is(b[[1]], "character")
+  expect_null(names(b))
+})
