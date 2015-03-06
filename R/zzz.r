8d9bfc24060b527a6c21cc07b1d660dc4b892213
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-06 13:26:25 -0800

    changed to_json fxn so it doesnt remove digits, affecting accuracy of the object added further example to geojson_json

--- R/zzz.r
+++ R/zzz.r
@@ -1,7 +1,7 @@
 tg_compact <- function(l) Filter(Negate(is.null), l)
 
 # to_json <- function(x, ...) structure(jsonlite::toJSON(x, ..., auto_unbox = TRUE), class=c('json','geo_json'))
-to_json <- function(x, ...) structure(jsonlite::toJSON(x, ..., auto_unbox = TRUE), class='json')
+to_json <- function(x, ...) structure(jsonlite::toJSON(x, ..., digits = 22, auto_unbox = TRUE), class='json')
 
 list_to_geo_list <- function(x, lat, lon, polygon, object, unnamed=FALSE){
   nn <- switch(object, FeatureCollection="features", GeometryCollection="geometries")
