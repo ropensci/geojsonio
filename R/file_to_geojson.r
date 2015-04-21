74c20084c85835aa6f8c1501f501f3c391fcd9b3
Scott Chamberlain <myrmecocystus@gmail.com>
2015-04-21 10:48:03 -0700

    fixed file_to_geojson, and using now within geojson_read fix #6, fix #40

--- R/file_to_geojson.r
+++ R/file_to_geojson.r
@@ -10,9 +10,9 @@
 #' @export
 #' @param input The file being uploaded, path to the file on your machine.
 #' @param method One of web or local. Matches on partial strings.
-#' @param destpath Destination for output geojson file. Defaults to your root
-#'    directory ('~/').
-#' @param outfilename The output file name, without file extension.
+#' @param output Destination for output geojson file. Defaults to root directory \code{~/}
+#' @param parse (logical) To parse geojson to data.frame like structures if possible. 
+#' Default: \code{FALSE}
 #' @description
 #' The web option uses the Ogre web API. Ogre currently has an output size limit of 15MB.
 #' See here \url{http://ogre.adc4gis.com/} for info on the Ogre web API.
@@ -26,22 +26,29 @@
 #' If you're having trouble rendering geoJSON files, ensure you have a valid
 #' geoJSON file by running it through a geoJSON linter \url{http://geojsonlint.com/}.
 #' @examples \dontrun{
-#' file <- '/Users/sacmac/Downloads/bison-Poa_annua-20140508-151800.kml'
+#' file <- system.file("examples", "norway_maple.kml", package = "geojsonio")
 #'
 #' # KML type file - using the web method
-#' file_to_geojson(input=file, method='web', outfilename='kml_web')
+#' file_to_geojson(input=file, method='web', output='kml_web')
+#' ## read into memory
+#' file_to_geojson(input=file, method='web', output = ":memory:")
+#' file_to_geojson(input=file, method='local', output = ":memory:")
 #'
 #' # KML type file - using the local method
-#' file_to_geojson(input=file, method='local', outfilename='kml_local')
+#' file_to_geojson(input=file, method='local', output='~/kml_local')
 #'
 #' # Shp type file - using the web method - input is a zipped shp bundle
-#' file <- '~/github/sac/bison.zip'
-#' file_to_geojson(file, method='web', outfilename='shp_web')
+#' file <- system.file("examples", "bison.zip", package = "geojsonio")
+#' file_to_geojson(file, method='web', output='shp_web')
 #'
 #' # Shp type file - using the local method - input is the actual .shp file
-#' file <- file.path('~/github/ropensci/datasets/misc/bison-Bison_bison-20140508-154311/',
-#' 'bison-Bison_bison-20140508-154311.shp')
-#' file_to_geojson(file, method='local', outfilename='shp_local')
+#' file <- system.file("examples", "bison.zip", package = "geojsonio")
+#' dir <- tempdir()
+#' unzip(file, exdir = dir)
+#' list.files(dir)
+#' shpfile <- file.path(dir, "bison-Bison_bison-20130704-120856.shp")
+#' file_to_geojson(shpfile, method='local', output='shp_local')
+#' file_to_geojson(shpfile, method='local', output=':memory:')
 #'
 #' # Get data and save map data
 #' splist <- c('Accipiter erythronemius', 'Junco hyemalis', 'Aix sponsa')
@@ -55,16 +62,14 @@
 #' # Put into a github repo to view on the web
 #' write.csv(datgeojson, '~/github/sac/mygeojson/rgbif_data.csv')
 #' file <- '~/github/sac/mygeojson/rgbif_data.csv'
-#' file_to_geojson(file, method='web', destpath='~/github/sac/mygeojson/',
-#'    outfilename='rgbif_data')
+#' file_to_geojson(file, method='web', destpath='~/github/sac/mygeojson/', output='rgbif_data')
 #'
 #' # Neighborhoods in the US
-#' url <- 'http://www.zillow.com/static/shp/ZillowNeighborhoods-OR.zip'
-#' url <- 'http://www.zillow.com/static/shp/ZillowNeighborhoods-MT.zip'
-#' out <- file_to_geojson(input=url, method='web', outfilename='zillow_mt')
+#' url <- 'http://www.nws.noaa.gov/geodata/catalog/national/data/ci08au12.zip'
+#' out <- file_to_geojson(input=url, method='web', output='cities')
 #' }
 
-file_to_geojson <- function(input, method = "web", destpath = "~/", outfilename = "myfile") {
+file_to_geojson <- function(input, method = "web", output = "~/", parse = FALSE) {
   method <- match.arg(method, choices = c("web", "local"))
   if (method == "web") {
     url <- "http://ogre.adc4gis.com/convert"
@@ -72,38 +77,76 @@ file_to_geojson <- function(input, method = "web", destpath = "~/", outfilename 
     tt <- POST(url, body = list(upload = upload_file(input)))
     stop_for_status(tt)
     out <- content(tt, as = "text")
-    fileConn <- file(paste0(destpath, outfilename, ".geojson"))
-    writeLines(out, fileConn)
-    close(fileConn)
-    message(paste0("Success! File is at ", destpath, outfilename, ".geojson"))
-    invisible(paste0(path.expand(destpath), outfilename, ".geojson"))
+    if (output == ":memory:") {
+      jsonlite::fromJSON(out, parse)
+    } else {
+      fileConn <- file(paste0(output, ".geojson"))
+      writeLines(out, fileConn)
+      close(fileConn)
+      message(paste0("Success! File is at ", output, ".geojson"))
+      invisible(paste0(path.expand(output), ".geojson"))
+    }
   } else {
-    fileext <- strsplit(input, "\\.")[[1]]
-    fileext <- fileext[length(fileext)]
+    fileext <- ftype(input)
+    mem <- ifelse(output == ":memory:", TRUE, FALSE)
+    output <- ifelse(output == ":memory:", tempfile(), output)
+    output <- path.expand(output)
     if (fileext == "kml") {
       my_layer <- ogrListLayers(input)
-      x <- readOGR(input, layer = my_layer[1])
-      unlink(paste0(destpath, outfilename, ".geojson"))
-      writeOGR(x, paste0(outfilename, ".geojson"), outfilename, driver = "GeoJSON")
-      message(paste0("Success! File is at ", destpath, outfilename, ".geojson"))
+      x <- readOGR(input, layer = my_layer[1], drop_unsupported_fields = TRUE)
+      unlink(paste0(output, ".geojson"))
+      writeOGR(x, paste0(output, ".geojson"), basename(output), driver = "GeoJSON", check_exists = FALSE)
+      if (mem) {
+        jsonlite::fromJSON(paste0(output, ".geojson"), parse)
+      } else {
+        message(paste0("Success! File is at ", output, ".geojson"))
+        invisible(paste0(output, ".geojson"))
+      }
     } else if (fileext == "shp") {
       x <- readShapeSpatial(input)
-      unlink(paste0(path.expand(destpath), outfilename, ".geojson"))
-      writeOGR(x, paste0(path.expand(destpath), outfilename, ".geojson"), outfilename,
-               driver = "GeoJSON")
-      message(paste0("Success! File is at ", path.expand(destpath), outfilename,
-                     ".geojson"))
-      invisible(paste0(path.expand(destpath), outfilename, ".geojson"))
+      unlink(paste0(output, ".geojson"))
+      writeOGR(x, paste0(output, ".geojson"), basename(output), driver = "GeoJSON", check_exists = FALSE)
+      if (mem) {
+        jsonlite::fromJSON(paste0(output, ".geojson"), parse)
+      } else {
+        message(paste0("Success! File is at ", output, ".geojson"))
+        invisible(paste0(output, ".geojson"))
+      }
+    } else if (fileext == "url") {
+      unlink(paste0(output, ".geojson"))
+      x <- readOGR(input, ogrListLayers(input))
+      unlink(paste0(output, ".geojson"))
+      writeOGR(x, paste0(output, ".geojson"), basename(output), driver = "GeoJSON", check_exists = FALSE)
+      if (mem) {
+        jsonlite::fromJSON(paste0(output, ".geojson"), parse)
+      } else {
+        message(paste0("Success! File is at ", output, ".geojson"))
+        invisible(paste0(output, ".geojson"))
+      }
     } else {
-      stop("only .shp and .kml files supported for now")
+      stop("only .shp, .kml, and url's are supported")
     }
   }
 }
 
+ftype <- function(z) {
+  if (is.url(z)) {
+    "url"
+  } else {
+    fileext <- strsplit(z, "\\.")[[1]]
+    fileext[length(z)]
+  }
+}
+
+is.url <- function(x){
+  grepl("https?://", x)
+}
+
 # If given a url for a zip file, download it give back a path to the temporary file
 handle_remote <- function(x){
-  if(!grepl('http://', x)){ x } else
-  {
+  if (!grepl('http://', x)) { 
+    x 
+  } else {
     tfile <- tempfile(fileext = ".zip")
     download.file(x, destfile = tfile)
     tfile
--- R/geojson_read.R
+++ R/geojson_read.R
@@ -3,36 +3,43 @@
 #' @export
 #'
 #' @param x Path to a local file or a URL.
-#' @param ... Further args passed on to \code{\link[rgdal]{readOGR}}
+#' @param method One of web or local. Matches on partial strings.
+#' @param parse (logical) To parse geojson to data.frame like structures if possible. 
+#' Default: \code{FALSE}
+#' @param ... Ignored
+#' @details Uses \code{\link{file_to_geojson}} internally.
 #'
 #' @examples \donttest{
 #' # From a file
-#' file <- system.file("examples", "california.geojson", package = "togeojson")
+#' file <- system.file("examples", "california.geojson", package = "geojsonio")
 #' out <- geojson_read(file)
 #' plot(out)
 #'
 #' # From a URL
 #' url <- "https://raw.githubusercontent.com/glynnbird/usstatesgeojson/master/california.geojson"
-#' geojson_read(url)
-#' geojson_read(url, stringsAsFactors=FALSE)
+#' geojson_read(url, method = "local")
 #' 
 #' # Use as.location first if you want
 #' geojson_read(as.location(file))
+#' 
+#' # use jsonlite to parse to data.frame structures where possible
+#' url <- "https://raw.githubusercontent.com/glynnbird/usstatesgeojson/master/california.geojson"
+#' geojson_read(url, method = "local", parse = TRUE)
 #' }
-geojson_read <- function(x, ...) {
+geojson_read <- function(x, method = "web", parse = FALSE, ...) {
   UseMethod("geojson_read")
 }
 
 #' @export
-geojson_read.character <- function(x, ...) { 
-  read_json(as.location(x), ...)
+geojson_read.character <- function(x, method = "web", parse = FALSE, ...) { 
+  read_json(as.location(x), method, parse, ...)
 }
 
 #' @export
-geojson_read.location <- function(x, ...) {
-  read_json(x, ...)
+geojson_read.location <- function(x, method = "web", parse = FALSE, ...) {
+  read_json(x, method, parse, ...)
 }
 
-read_json <- function(x, ...) {
-  readOGR(x, ogrListLayers(x), ...)
+read_json <- function(x, method, parse, ...) {
+  file_to_geojson(x, method, output = ":memory:", parse)
 }
