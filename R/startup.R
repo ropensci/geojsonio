10b8eab16fec9055ac3d0899d0c8972ad14fafb9
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-06 17:20:05 -0800

    added startup message to warn about rgdal version, #69

---
+++ R/startup.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("\n\nWe recommend using rgdal v1.1-1 or greater, but we don't require it\nrgdal::writeOGR in previous versions didn't write\nmultipolygon objects to geojson correctly.\nSee https://stat.ethz.ch/pipermail/r-sig-geo/2015-October/023609.html")
+}
