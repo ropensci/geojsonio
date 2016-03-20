1244fdc0f2b7cfa4a00f571c1263b943decd2700
Scott Chamberlain <myrmecocystus@gmail.com>
2016-03-20 09:21:20 -0700

    eg tweak

--- R/cwgr.R
+++ R/cwgr.R
@@ -5,6 +5,9 @@
 #' library("rgdal")
 #' cities <- readOGR(system.file("vectors", package = "rgdal")[1], "cities")
 #' cwgr(cities[1:10,], "cities")
+#' 
+#' coordinates(states) <- ~long+lat
+#' cwgr(states[1:100,], "states")
 #' }
 cwgr <- function(obj, layer) {
   capturedWriteOGR(obj, object.size(obj), layer, writeOGR)
