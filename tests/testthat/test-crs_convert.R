86c5f15961165e332913f37a15f24f396d3bffe5
ateucher <andy.teucher@gmail.com>
2017-01-13 22:40:13 -0800

    Initial tests for crs conversion functions

--- tests/testthat/test-crs_convert.R
+++ tests/testthat/test-crs_convert.R
@@ -1,25 +1,39 @@
-library(sf)
+context("detect and convert crs")
+suppressPackageStartupMessages(library(sf))
+suppressPackageStartupMessages(library(sp))
+
 sfc <-  st_sfc(st_point(c(0,0)), st_point(c(1,1)))
 sf <-  st_sf(a = 1:2, geom = sfc)
-st_crs(sf) <-  4326
-detect_convert_crs(sf)
-
-st_crs(sf) <- 3005
-detect_convert_crs(sf)
-
-st_crs(sfc) <- 4326
-detect_convert_crs(sfc)
-
-st_crs(sfc) <- 3005
-detect_convert_crs(sfc)
-
-library(sp)
 pts = cbind(1:5, 1:5)
 df = data.frame(a = 1:5)
-
 spdf <- SpatialPointsDataFrame(pts, df)
-proj4string(spdf) <- CRS("+init=epsg:4326")
-detect_convert_crs(spdf)
 
-proj4string(spdf) <- CRS("+init=epsg:3005")
-detect_convert_crs(spdf)
+test_that("works with sf", {
+  suppressWarnings(st_crs(sf) <-  4326)
+  expect_equal(st_crs(detect_convert_crs(sf))[["proj4string"]],
+               "+proj=longlat +datum=WGS84 +no_defs")
+  
+  suppressWarnings(st_crs(sf) <- 3005)
+  expect_equal(st_crs(detect_convert_crs(sf))[["proj4string"]],
+               "+proj=longlat +datum=WGS84 +no_defs")
+})
+
+test_that("works with sf", {
+  suppressWarnings(st_crs(sfc) <-  4326)
+  expect_equal(st_crs(detect_convert_crs(sfc))[["proj4string"]],
+               "+proj=longlat +datum=WGS84 +no_defs")
+  
+  suppressWarnings(st_crs(sfc) <- 3005)
+  expect_equal(st_crs(detect_convert_crs(sfc))[["proj4string"]],
+               "+proj=longlat +datum=WGS84 +no_defs")
+})
+
+test_that("works with spatial", {
+suppressWarnings(proj4string(spdf) <- CRS("+init=epsg:4326"))
+expect_equal(proj4string(detect_convert_crs(spdf)), 
+             "+init=epsg:4326 +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
+suppressWarnings(proj4string(spdf) <- CRS("+init=epsg:3005"))
+expect_equal(proj4string(detect_convert_crs(spdf)), 
+             "+init=epsg:4326 +proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+})
