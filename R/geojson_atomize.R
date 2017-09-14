76c4384c51f84c489520691172f6dd72cdb06983
Scott Chamberlain <myrmecocystus@gmail.com>
2017-09-14 15:34:33 -0700

    fix #119 add combine parameter to geojson_atomize, add character and json methods to fxn

--- R/geojson_atomize.R
+++ R/geojson_atomize.R
@@ -2,11 +2,20 @@
 #' 
 #' @importFrom jqr jq
 #' @export
-#' @param x input object, either json or list
+#' @param x (geo_list/geo_json/json/character) input object, either 
+#' \code{geo_json}, \code{geo_list}, \code{json}, or \code{character} class.
+#' If \code{character}, must be valid JSON
+#' @param combine (logical) only applies to \code{geo_json/json} type inputs.
+#' combine valid JSON objects into a single valid JSON object. Default: 
+#' \code{TRUE}
+#' 
 #' @return same class as input object, but modified
-#' @details outputs valid JSON - when a FeatureCollection is split into 
-#' many Feature's, those are put into a JSON array making a valid JSON 
-#' object
+#' 
+#' @details A FeatureCollection is split into many Feature's, and 
+#' a GeometryCollection is split into many geometries
+#' 
+#' Internally we use \pkg{jqr} for JSON parsing
+#' 
 #' @examples
 #' ################# lists 
 #' # featurecollection -> features
@@ -35,29 +44,48 @@
 #'                list(latitude=30, longitude=130, marker="blue"))
 #' (x <- geojson_json(mylist))
 #' geojson_atomize(x)
+#' geojson_atomize(x, FALSE)
 #' 
 #' # geometrycollection -> geometries
 #' mylist <- list(list(latitude=30, longitude=120, marker="red"),
 #'                list(latitude=30, longitude=130, marker="blue"))
 #' (x <- geojson_json(mylist, type = "GeometryCollection"))
 #' geojson_atomize(x)
+#' geojson_atomize(x, FALSE)
 #' 
 #' # sf class
 #' library(sf)
-#' p1 <- rbind(c(0,0), c(1,0), c(3,2), c(2,4), c(1,4), c(0,0))
-#' poly <- rbind(c(1,1), c(1,2), c(2,2), c(1,1))
-#' poly_sfg <- st_polygon(list(p1))
-#' (x <- geojson_json(poly_sfg))
+#' nc <- st_read(system.file("shape/nc.shp", package="sf"), quiet = TRUE)
+#' (x <- geojson_json(nc))
 #' geojson_atomize(x)
-geojson_atomize <- function(x) UseMethod("geojson_atomize")
+#' geojson_atomize(x, FALSE)
+#' 
+#' ################# character
+#' # featurecollection -> features
+#' mylist <- list(list(latitude=30, longitude=120, marker="red"),
+#'                list(latitude=30, longitude=130, marker="blue"))
+#' (x <- geojson_json(mylist))
+#' geojson_atomize(unclass(x))
+geojson_atomize <- function(x, combine = TRUE) UseMethod("geojson_atomize")
 
 #' @export
-geojson_atomize.default <- function(x) {
+geojson_atomize.default <- function(x, combine = TRUE) {
   stop("no 'geojson_atomize' method for ", class(x))
 }
 
 #' @export
-geojson_atomize.geo_json <- function(x) {
+geojson_atomize.geo_json <- function(x, combine = TRUE) {
+  geojson_atomize(unclass(x), combine = combine)
+}
+
+#' @export
+geojson_atomize.character <- function(x, combine = TRUE) {
+  if (!jsonlite::validate(x)) stop("'x' not valid JSON")
+  geojson_atomize(structure(x, class = "json"), combine = combine)
+}
+
+#' @export
+geojson_atomize.json <- function(x, combine = TRUE) {
   type <- stripjqr(jqr::jq(unclass(x), ".type"))
   keys <- stripjqr(jqr::jq(unclass(x), "keys[]"))
   if (type %in% c('Point', 'MultiPoint', 
@@ -72,11 +100,11 @@ geojson_atomize.geo_json <- function(x) {
       tmp <- jqr::jq(unclass(x), ".geometries[]")
     }
   }
-  structure(jqr::combine(tmp), class = "json")
+  structure(if (combine) jqr::combine(tmp) else tmp, class = "json")
 }
 
 #' @export
-geojson_atomize.geo_list <- function(x) {
+geojson_atomize.geo_list <- function(x, combine = TRUE) {
   if (x$type %in% c('Point', 'MultiPoint', 
                     'Polygon', 'MultiPolygon', 
                     'LineString', 'MultiLineString')) {
--- tests/testthat/test-geojson_atomize.R
+++ tests/testthat/test-geojson_atomize.R
@@ -11,11 +11,24 @@ test_that("geojson_atomize works with list inputs", {
   expect_equal(vapply(aa, "[[", "", "type"), rep("Feature", 2))
 })
 
-test_that("geojson_atomize works with json inputs", {
+test_that("geojson_atomize works with geo_json inputs", {
   mylist <- list(list(latitude = 30, longitude = 120, marker = "red"),
                  list(latitude = 30, longitude = 130, marker = "blue"))
   js <- geojson_json(mylist)
   aa <- geojson_atomize(js)
+  aajs <- jsonlite::fromJSON(aa, FALSE)
+  
+  expect_is(aa, "json")
+  # outputs are features
+  expect_equal(vapply(aajs, "[[", "", "type"), rep("Feature", 2))
+})
+
+test_that("geojson_atomize works with character inputs", {
+  mylist <- list(list(latitude = 30, longitude = 120, marker = "red"),
+                 list(latitude = 30, longitude = 130, marker = "blue"))
+  js <- geojson_json(mylist)
+  jsc <- unclass(js)
+  aa <- geojson_atomize(jsc)
   aajs <- jsonlite::fromJSON(aa, FALSE)
   
   expect_is(aa, "json")
