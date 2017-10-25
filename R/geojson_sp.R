ced658e765dd2a0dadf505c01ad43acc3bfa784c
Scott Chamberlain <myrmecocystus@gmail.com>
2017-10-25 11:47:30 -0700

    fix #117 add topojson_list method, bump patch version udpate pkg level man file with new topojson methods

--- R/geojson_sp.R
+++ R/geojson_sp.R
@@ -1,25 +1,25 @@
-#' Convert output of geojson_list or geojson_json to spatial classes
-#' 
+#' Convert output of \code{geojson_list} or \code{geojson_json} to spatial classes
+#'
 #' @export
 #' @param x Object of class \code{geo_list} or \code{geo_json}
-#' @param disambiguateFIDs (logical) default \code{FALSE}, if \code{TRUE}, and FID 
-#' values are not unique, they will be set to unique values 1:N for N features; 
+#' @param disambiguateFIDs (logical) default \code{FALSE}, if \code{TRUE}, and FID
+#' values are not unique, they will be set to unique values 1:N for N features;
 #' problem observed in GML files
 #' @param ... Further args passed on to \code{\link[rgdal]{readOGR}}
-#' 
+#'
 #' @return A spatial class object, see Details.
-#' @details The spatial class object returned will depend on the input GeoJSON. 
-#' Sometimes you will get back a \code{SpatialPoints} class, and sometimes a 
-#' \code{SpatialPolygonsDataFrame} class, etc., depending on what the 
-#' structure of the GeoJSON. 
-#' 
-#' The reading and writing of the CRS to/from geojson is inconsistent. You can 
-#' directly set the CRS by passing a valid PROJ4 string to the \code{p4s} argument in 
+#' @details The spatial class object returned will depend on the input GeoJSON.
+#' Sometimes you will get back a \code{SpatialPoints} class, and sometimes a
+#' \code{SpatialPolygonsDataFrame} class, etc., depending on what the
+#' structure of the GeoJSON.
+#'
+#' The reading and writing of the CRS to/from geojson is inconsistent. You can
+#' directly set the CRS by passing a valid PROJ4 string to the \code{p4s} argument in
 #' \code{\link[rgdal]{readOGR}}.
-#' 
+#'
 #' @examples \dontrun{
 #' library(sp)
-#' 
+#'
 #' # geo_list ------------------
 #' ## From a numeric vector of length 2 to a point
 #' vec <- c(-99.74,32.45)
@@ -36,19 +36,19 @@
 #' vecs <- list(c(100.0,0.0), c(101.0,0.0), c(101.0,1.0), c(100.0,1.0), c(100.0,0.0))
 #' geojson_list(vecs, geometry="polygon") %>% geojson_sp
 #' geojson_list(vecs, geometry="polygon") %>% geojson_sp %>% plot
-#' 
+#'
 #' # geo_json ------------------
 #' ## from point
 #' geojson_json(c(-99.74,32.45)) %>% geojson_sp
 #' geojson_json(c(-99.74,32.45)) %>% geojson_sp %>% plot
-#' 
+#'
 #' # from featurecollectino of points
 #' geojson_json(us_cities[1:2,], lat='lat', lon='long') %>% geojson_sp
 #' geojson_json(us_cities[1:2,], lat='lat', lon='long') %>% geojson_sp %>% plot
-#' 
+#'
 #' # Set the CRS via the p4s argument
 #' geojson_json(us_cities[1:2,], lat='lat', lon='long') %>% geojson_sp(p4s = "+init=epsg:4326")
-#' 
+#'
 #' # json ----------------------
 #' x <- geojson_json(us_cities[1:2,], lat='lat', lon='long')
 #' geojson_sp(x)
@@ -73,6 +73,6 @@ geojson_sp.json <- function(x, disambiguateFIDs = TRUE, ...) {
 }
 
 tosp <- function(x, disambiguateFIDs, ...) {
-  rgdal::readOGR(x, layer = "OGRGeoJSON", disambiguateFIDs = disambiguateFIDs, 
-                 verbose = FALSE, ...)  
+  rgdal::readOGR(x, layer = "OGRGeoJSON", disambiguateFIDs = disambiguateFIDs,
+                 verbose = FALSE, ...)
 }
