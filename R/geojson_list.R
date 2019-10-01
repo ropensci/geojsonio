12a58f0b605f07cc2b8bbc3f0025216816aaca5f
Chris Michael Jones <cjones1688@gmail.com>
2019-10-01 13:55:45 -0400

    updated documentation to include precision parameter

--- R/geojson_list.R
+++ R/geojson_list.R
@@ -11,6 +11,9 @@
 #' GeometryCollection.
 #' @param group (character) A grouping variable to perform grouping for polygons - doesn't apply
 #' for points
+#' @param precision desired number of decimal places for the coordinates in the
+#'   geojson file. Using fewer decimal places can decrease file sizes (at the
+#'   cost of precision).
 #' @param convert_wgs84 Should the input be converted to the \href{https://tools.ietf.org/html/rfc7946}{standard coordinate reference system defined for GeoJSON} (geographic coordinate reference system, using the WGS84 datum, with longitude and latitude units of decimal degrees; EPSG: 4326). Default is \code{FALSE} though this may change in a future package version. This will only work for \code{sf} or \code{Spatial} objects with a CRS already defined. If one is not defined but you know what it is, you may define it in the \code{crs} argument below.
 #' @param crs The CRS of the input if it is not already defined. This can be an epsg code as a four or five digit integer or a valid proj4 string. This argument will be ignored if \code{convert_wgs84} is \code{FALSE} or the object already has a CRS.
 #' @param ... Ignored
