d655e71756001924da79d86797c036a49e84ca92
Mike Mahoney <mike.mahoney.218@gmail.com>
2023-08-19 07:33:43 -0400

    Remove unused V8 call

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,11 +1,7 @@
 #' @importFrom V8 new_context
-ct <- NULL
 ext <- NULL
 topo <- NULL
 .onLoad <- function(libname, pkgname) {
-  ct <<- V8::v8()
-  ct$source(system.file("js/geojsonhint.js", package = pkgname))
-
   ext <<- V8::v8()
   ext$source(system.file("js/turf_extent.js", package = pkgname))
 
